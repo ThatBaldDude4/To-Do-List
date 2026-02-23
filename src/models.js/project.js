48183baa3b7ad2fb6a40256269a947c3865330a2
Taylor V. <taylorv0004@gmail.com>
2026-02-22 20:54:47 -0600

    add project module

---
+++ src/models.js/project.js
@@ -0,0 +1,16 @@
+export class Project {
+    constructor(title) {
+        this.title = title;
+        this.items = [];
+        this.id = crypto.randomUUID();
+    }
+
+    addItem(item) {
+        this.items.push(item);
+    }
+
+    deleteItem(item) {
+        const index = this.items.indexOf(item);
+        this.items.splice(index, 1);
+    }
+}
