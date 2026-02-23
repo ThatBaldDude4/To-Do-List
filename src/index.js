f8c1ebe40a5dd1f8c2e6f04f1518317d972cdccb
Taylor V. <taylorv0004@gmail.com>
2026-02-23 08:17:10 -0600

    remove testing code

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 import {Item} from "./models/item.js";
 import {Project} from "./models/project.js";
 
+function createProject(title) {
+    const project = new Project({title});
+    return project;
+};
+
+function createItem(title, description) {
+    const item = new Item({title, description});
+    return item;
+};
+
+function cleanUpItems(project, id) {
+    project.cleanItem(id);
+    return;
+}
+
 const project1 = new Project({title: "first project"})
 
-let firstItem = new Item({
-    title: "Test",
-    description: "new desc"
-})
-let secondItem = new Item({
-    title: "Test",
-    description: "new desc"
-})
-let thirdItem = new Item({
-    title: "Test",
-    description: "new desc"
-})
-
 project1.addItem(firstItem)
+console.log(firstItem);
+firstItem.updateItem({title: "newTitle", description: "new desc"})
+console.log(firstItem);
 let updatedItem = firstItem.getNewUpdate({title: "newTitle"})
-project1.addItem(updatedItem)
-console.log(project1.items);
-
+console.log();
 // this is where you'll add the modules together
 // try to seperate as much code as possible following SOLID
