fa3e1143a842d7dd156fd05c8999b2284a5ddbbf
ThatBaldDude4 <taylorv0004@gmail.com>
2026-02-24 20:32:37 -0600

    create projectForm func

---
+++ src/ui/projectForm.js
@@ -0,0 +1,33 @@
+export function projectForm() {
+    const container = document.createElement("div");
+    const heading = document.createElement("h2");
+    const titlelabel = document.createElement("label");
+    const titleInput = document.createElement("input");
+    const submitBtn = document.createElement("button");
+    const cancelBtn = document.createElement("button");
+
+    heading.textContent = "Project Form";
+
+    titlelabel.textContent = "Project Title:";
+    titlelabel.classList.add("title-label");
+
+    titleInput.type = "input";
+    titleInput.classList.add("title-input");
+
+    submitBtn.textContent = "SUBMIT";
+    submitBtn.classList.add("submit-btn");
+    submitBtn.type = "submit";
+
+    cancelBtn.textContent = "CANCEL";
+    cancelBtn.classList.add("cancel-btn");
+    cancelBtn.type = "button";
+
+    titlelabel.appendChild(titleInput);
+
+    container.appendChild(heading);
+    container.appendChild(titlelabel);
+    container.appendChild(submitBtn);
+    container.appendChild(cancelBtn);
+
+    return container;
+};
