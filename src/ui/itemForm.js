e412ef242756ca0978381290576ddcd7e6dca083
ThatBaldDude4 <127453013+ThatBaldDude4@users.noreply.github.com>
2026-02-25 18:24:08 +0000

    add itemForm() function

---
+++ src/ui/itemForm.js
@@ -0,0 +1,43 @@
+export function itemForm() {
+    const container = document.createElement("div");
+    const heading = document.createElement("h2");
+    const titlelabel = document.createElement("label");
+    const titleInput = document.createElement("input");
+    const descriptionLabel = document.createElement("label");
+    const descriptionInput = document.createElement("input");
+    const submitBtn = document.createElement("button");
+    const cancelBtn = document.createElement("button");
+
+    heading.textContent = "Item Form";
+
+    titlelabel.textContent = "Item Title:";
+    titleInput.classList.add("item-title-label");
+
+    titleInput.type = "input";
+    titleInput.classList.add("item-title-input");
+
+    descriptionLabel.textContent = "Item description";
+    descriptionLabel.classList.add("item-description-label");
+
+    descriptionInput.type = "input";
+    descriptionInput.classList.add("item-description-input");
+
+    submitBtn.textContent = "SUBMIT";
+    submitBtn.setAttribute("data-action", "submit-item");
+    submitBtn.type = "submit";
+
+    cancelBtn.textContent = "CANCEL";
+    cancelBtn.setAttribute("data-action", "cancel-form");
+    cancelBtn.type = "button";
+
+    titlelabel.appendChild(titleInput);
+    descriptionLabel.appendChild(descriptionInput);
+
+    container.appendChild(heading);
+    container.appendChild(titlelabel);
+    container.appendChild(descriptionLabel);
+    container.appendChild(submitBtn);
+    container.appendChild(cancelBtn);
+
+    return container;
+}
